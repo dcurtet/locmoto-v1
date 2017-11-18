5090c2aaafb25495beb40c5424f0e4c9e8600202
Damien <damien@mac.home>
2017-11-18 11:42:40 +0100

    init

---
+++ src/AppBundle/Entity/Repository/DocumentRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class DocumentRepository
+ * @package AppBundle\Entity\Repository
+ */
+
+class DocumentRepository extends EntityRepository
+{
+
+
+}
